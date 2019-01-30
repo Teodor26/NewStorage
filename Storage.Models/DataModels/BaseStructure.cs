ed8388cae64000c49932a2e135eb3d6830067ad9
Teodor26 <teodorbear26@gmail.com>
2019-01-30 23:52:31 +0200

    Models has been made.

---
+++ Storage.Models/DataModels/BaseStructure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Models.DataModels
+{
+    abstract class  BaseStructure
+    {
+        public DateTime data = new DateTime();
+
+        public int Amount { get; set; }
+
+        public int GoodId { get; set; }
+    }
+}
